41427f03378d75439c6f0ce126a96628b80584d6
Miguel ALmeida <miguelcjalmeida@gmail.com>
2020-11-06 02:07:32 -0300

    add projection calculation to show possibility of turning the game around

--- index.js
+++ index.js
@@ -1,118 +1,123 @@
 ;(function () {
-    const votesByStateCache = {}
-  
-    init()
-  
-    function init() {
-      let $states = document.querySelectorAll('.fKE5Bb')
-      let hasNewVotes = false
-  
-      $states.forEach(($state) => initState($state))
-      keepCheckingChanges()
-  
-      function initState($state) {
-        const stateName = getStateName($state)
-        votesByStateCache[stateName] = [0, 0]
-      }
-  
-      function checkStateVoteChanges($state) {
-        if (didAnyVoteHappenForState($state)) {
-          console.log(getStateVoteUpdateMessage($state))
-          votesByStateCache[getStateName($state)] = getStateVotes($state)
-          hasNewVotes = true
-        }
-      }
-  
-      function checkChanges() {
-        $states = document.querySelectorAll('.fKE5Bb')
-        $states.forEach(($state) => checkStateVoteChanges($state))
-        if (hasNewVotes) {
-          alert('New votes! Check console.log')
-          hasNewVotes = false
-        }
-      }
-  
-      function keepCheckingChanges() {
-        const repetition = () => {
-          console.log('checking for new votes..')
-          checkChanges()
-        }
-  
-        repetition()
-        setInterval(repetition, 60000)
-      }
-    }
-  
-    function getStateVoteUpdateMessage($state) {
+  const votesByStateCache = {}
+
+  init()
+
+  function init() {
+    let $states = document.querySelectorAll('.fKE5Bb')
+    let hasNewVotes = false
+
+    $states.forEach($state => initState($state))
+    keepCheckingChanges()
+
+    function initState($state) {
       const stateName = getStateName($state)
-      const currentVotes = getStateVotes($state)
-      const cachedVotes = votesByStateCache[stateName]
-      const votesDiff = currentVotes[0] - currentVotes[1]
-  
-      const line1 = `${stateName} got new votes!`
-      const line2 = buildVoteLine('Biden', cachedVotes[0], currentVotes[0])
-      const line3 = buildVoteLine('Trump', cachedVotes[1], currentVotes[1])
-      const line4 =
-        votesDiff >= 0 ? `Trump needs ${f(votesDiff)} votes` : `Biden needs ${f(-votesDiff)} votes`
-  
-      return `${line1}\n${line2}\n${line3}\n${line4}`
-  
-      function buildVoteLine(candidate, oldVote, currentVote) {
-        const oldVoteFormatted = f(oldVote)
-        const currentVoteFormatted = f(currentVote)
-        const diffPrefix = currentVote > oldVote ? '+' : ''
-        const diffFormatted = `${diffPrefix}${f(currentVote - oldVote)}`
-  
-        return `${candidate}: ${oldVoteFormatted} -> ${currentVoteFormatted} (${diffFormatted})`
+      votesByStateCache[stateName] = [0, 0]
+    }
+
+    function checkStateVoteChanges($state) {
+      if (didAnyVoteHappenForState($state)) {
+        console.log(getStateVoteUpdateMessage($state))
+        votesByStateCache[getStateName($state)] = getStateVotes($state)
+        hasNewVotes = true
       }
     }
-  
-    function f(number) {
-      return number.toLocaleString()
+
+    function checkChanges() {
+      $states = document.querySelectorAll('.fKE5Bb')
+      $states.forEach($state => checkStateVoteChanges($state))
+      if (hasNewVotes) {
+        alert('New votes! Check console.log')
+        hasNewVotes = false
+      }
     }
-  
-    function didAnyVoteHappenForState($state) {
-      const stateName = getStateName($state)
-      const currentVotes = getStateVotes($state)
-      const cacheVotes = votesByStateCache[stateName]
-      return !areVotesEqual(currentVotes, cacheVotes)
+
+    function keepCheckingChanges() {
+      const repetition = () => {
+        console.log('checking for new votes..')
+        checkChanges()
+      }
+
+      repetition()
+      setInterval(repetition, 5000)
     }
-  
-    function areVotesEqual($votes1, $votes2) {
-      return $votes1[0] == $votes2[0] && $votes1[1] === $votes2[1]
+  }
+
+  function getStateVoteUpdateMessage($state) {
+    const biden = 'Biden'
+    const trump = 'Trump'
+    const stateName = getStateName($state)
+    const currentVotes = getStateVotes($state)
+    const cachedVotes = votesByStateCache[stateName]
+    const votesDiff = currentVotes[0] - currentVotes[1]
+    const isBidenWinning = votesDiff >= 0
+    const losingCandidate = isBidenWinning ? trump : biden
+    const bidenNewVotes = currentVotes[0] - cachedVotes[0]
+    const trumpNewVotes = currentVotes[1] - cachedVotes[1]
+    const loserNewVotes = isBidenWinning ? trumpNewVotes : bidenNewVotes
+
+    const projectionInFloat = Math.abs(votesDiff) / (loserNewVotes / (bidenNewVotes + trumpNewVotes))
+    const projection = parseInt(projectionInFloat, 10)
+    const isProjectionVisible = (isBidenWinning && trumpNewVotes > bidenNewVotes) || (!isBidenWinning && bidenNewVotes > trumpNewVotes)
+
+    const line1 = `${stateName} got new votes!`
+    const line2 = buildVoteLine(biden, cachedVotes[0], currentVotes[0])
+    const line3 = buildVoteLine(trump, cachedVotes[1], currentVotes[1])
+    const line4 = votesDiff >= 0 ? `${trump} needs ${f(votesDiff)} votes` : `${biden} needs ${f(-votesDiff)} votes`
+    const line5 = isProjectionVisible ? `By projection, ${losingCandidate} could be winning after ${f(projection)} votes` : ''
+
+    return `${line1}\n${line2}\n${line3}\n${line4}\n${line5}`
+
+    function buildVoteLine(candidate, oldVote, currentVote) {
+      const oldVoteFormatted = f(oldVote)
+      const currentVoteFormatted = f(currentVote)
+      const diffPrefix = currentVote > oldVote ? '+' : ''
+      const diffFormatted = `${diffPrefix}${f(currentVote - oldVote)}`
+
+      return `${candidate}: ${oldVoteFormatted} -> ${currentVoteFormatted} (${diffFormatted})`
     }
-  
-    function getStateName($state) {
-      return $state.querySelector(':scope > div > span').textContent
-    }
-  
-    function getStateVotes($state) {
-      return [getBidenVotes($state), getTrumpVotes($state)]
-    }
-  
-    function getBidenVotes($state) {
-      const $votes = $state.querySelectorAll(':scope > div')[1]
-      const $biden = $votes.querySelectorAll(':scope > div')[0]
-      return getSectionVotes($biden)
-    }
-  
-    function getTrumpVotes($state) {
-      const $votes = $state.querySelectorAll(':scope > div')[1]
-      const $trump = $votes.querySelectorAll(':scope > div')[1]
-      return getSectionVotes($trump)
-    }
-  
-    function getSectionVotes($voteSection) {
-      return parseInt(
-        $voteSection.querySelectorAll(':scope > span')[1].textContent.replace(/[\.\,]/g, ''),
-        10,
-      )
-    }
-  
-    function getState($name) {
-      return document
-        .evaluate(`//span[text()="${$name}"]`, document, null, XPathResult.ANY_TYPE, null)
-        .iterateNext().parentNode.parentNode
-    }
-  })()
-  
+  }
+
+  function f(number) {
+    return number.toLocaleString()
+  }
+
+  function didAnyVoteHappenForState($state) {
+    const stateName = getStateName($state)
+    const currentVotes = getStateVotes($state)
+    const cacheVotes = votesByStateCache[stateName]
+    return !areVotesEqual(currentVotes, cacheVotes)
+  }
+
+  function areVotesEqual($votes1, $votes2) {
+    return $votes1[0] == $votes2[0] && $votes1[1] === $votes2[1]
+  }
+
+  function getStateName($state) {
+    return $state.querySelector(':scope > div > span').textContent
+  }
+
+  function getStateVotes($state) {
+    return [getBidenVotes($state), getTrumpVotes($state)]
+  }
+
+  function getBidenVotes($state) {
+    const $votes = $state.querySelectorAll(':scope > div')[1]
+    const $biden = $votes.querySelectorAll(':scope > div')[0]
+    return getSectionVotes($biden)
+  }
+
+  function getTrumpVotes($state) {
+    const $votes = $state.querySelectorAll(':scope > div')[1]
+    const $trump = $votes.querySelectorAll(':scope > div')[1]
+    return getSectionVotes($trump)
+  }
+
+  function getSectionVotes($voteSection) {
+    return parseInt($voteSection.querySelectorAll(':scope > span')[1].textContent.replace(/[\.\,]/g, ''), 10)
+  }
+
+  function getState($name) {
+    return document.evaluate(`//span[text()="${$name}"]`, document, null, XPathResult.ANY_TYPE, null).iterateNext().parentNode.parentNode
+  }
+})()
