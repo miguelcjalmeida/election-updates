0a5a8d22246367f14575108c41d3af133282b0d7
Miguel ALmeida <miguelcjalmeida@gmail.com>
2020-11-06 04:11:58 -0300

    refactor the message display function, replacing a conditional by variables

--- index.js
+++ index.js
@@ -58,8 +58,8 @@ var SetStateVotes = null
     const stateName = getStateName($state)
     const currentVotes = getStateVotes($state)
     const cachedVotes = votesByStateCache[stateName]
-    const votesDiff = currentVotes[0] - currentVotes[1]
-    const isBidenWinning = votesDiff >= 0
+    const votesDiff = Math.abs(currentVotes[0] - currentVotes[1])
+    const isBidenWinning = currentVotes[0] >= currentVotes[1]
     const losingCandidate = isBidenWinning ? trump : biden
     const bidenNewVotes = currentVotes[0] - cachedVotes[0]
     const trumpNewVotes = currentVotes[1] - cachedVotes[1]
@@ -68,7 +68,7 @@ var SetStateVotes = null
     const trumpPercNewVotes = trumpNewVotes / totalNewVotes
     const bidenPercNewVotes = bidenNewVotes / totalNewVotes
     const loserDiffPercNewVote = isBidenWinning ? trumpPercNewVotes - bidenPercNewVotes : bidenPercNewVotes - trumpPercNewVotes
-    const projectionInFloat = Math.abs(votesDiff) / loserDiffPercNewVote
+    const projectionInFloat = votesDiff / loserDiffPercNewVote
 
     const projection = parseInt(projectionInFloat, 10)
     const isProjectionVisible = (isBidenWinning && trumpNewVotes > bidenNewVotes) || (!isBidenWinning && bidenNewVotes > trumpNewVotes)
@@ -76,7 +76,7 @@ var SetStateVotes = null
     const line1 = `${stateName} got new votes!`
     const line2 = buildVoteLine(biden, cachedVotes[0], currentVotes[0])
     const line3 = buildVoteLine(trump, cachedVotes[1], currentVotes[1])
-    const line4 = isBidenWinning ? `${trump} needs ${f(votesDiff)} votes` : `${biden} needs ${f(-votesDiff)} votes`
+    const line4 = `${losingCandidate} needs ${f(votesDiff)} votes`
     const line5 = isProjectionVisible ? `By projection, ${losingCandidate} could be winning after ${f(projection)} more votes counted` : ''
 
     return `${line1}\n${line2}\n${line3}\n${line4}\n${line5}`
